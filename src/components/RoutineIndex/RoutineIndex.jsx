20901b46bc386feb3d5f5481cf480d54c553ce8b
William Stull II <williamstull@gmail.com>
2021-08-10 09:08:10 -0700

    Deploy

--- src/components/RoutineIndex/RoutineIndex.jsx
+++ src/components/RoutineIndex/RoutineIndex.jsx
@@ -1,8 +1,8 @@
 import RoutineCard from '../RoutineCard/RoutineCard';
 
-export default function RoutineIndex({ routines }) {
+export default function RoutineIndex({ routines, setCurrRoutine }) {
     const routines = routines.map(routine => 
-        <RoutineCard routine={routine}/>
+        <RoutineCard routine={routine} setCurrRoutine={setCurrRoutine}/>
     );
 
     return (
