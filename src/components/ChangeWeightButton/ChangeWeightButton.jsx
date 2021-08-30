1ddc82fd19d9ab22a116cbb0570b3e66b1b2e24a
William Stull II <williamstull@gmail.com>
2021-08-30 13:37:11 -0700

    Clean up, clean up, it's time to clean up

--- src/components/ChangeWeightButton/ChangeWeightButton.jsx
+++ src/components/ChangeWeightButton/ChangeWeightButton.jsx
@@ -1,12 +1,8 @@
 import TextField from '@material-ui/core/TextField';
-import InputAdornment from '@material-ui/core/InputAdornment';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import CardContent from '@material-ui/core/CardContent';
-import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button'
 import { useState } from 'react'
 import * as routineAPI from '../../utilities/routines-api'
--- src/components/WorkoutDetail/WorkoutDetail.jsx
+++ src/components/WorkoutDetail/WorkoutDetail.jsx
@@ -1,22 +1,12 @@
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import Button from '@material-ui/core/Button'
-import * as routineUtil from '../../utilities/routines-service'
 import * as routineAPI from '../../utilities/routines-api'
 import * as routineInfoAPI from '../../utilities/routine-info-api'
 import Fab from '@material-ui/core/Fab'
 import AddIcon from '@material-ui/icons/Add'
-import TextField from '@material-ui/core/TextField';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
-import { useState } from 'react'
 import ChangeWeightButton from '../ChangeWeightButton/ChangeWeightButton';
 
 const useStyles = makeStyles({
@@ -83,14 +73,8 @@ export default function WorkoutDetail({ workout, routineInfo, currRoutine, setWo
             };
         }
         e.currentTarget.innerHTML = e.currentTarget.value;
-        console.log(e.currentTarget.value == reps);
-        console.log(e.currentTarget.value + ' ' + reps)
         if (e.currentTarget.value == reps) completeArr[upperIdx][idx] = true;
         else completeArr[upperIdx][idx] = false;
-        console.log('upperIdx: ' + upperIdx)
-        console.log('idx: ' + idx)
-        console.log('arr at those indexs' + completeArr[upperIdx][idx]);
-        console.log(completeArr);
     };
 
     function handleSets(exercise, idx) {
@@ -108,7 +92,6 @@ export default function WorkoutDetail({ workout, routineInfo, currRoutine, setWo
             innerArr.push(false);
         }
         completeArr.push(innerArr);
-        console.log(completeArr);
         buttons.push(
             <Grid item>
                 <Card className={classes.control}>
@@ -126,7 +109,6 @@ export default function WorkoutDetail({ workout, routineInfo, currRoutine, setWo
                 </Card>
             </Grid>
         )
-        console.log(buttons);
         return buttons;
     }
     const buttonsArray = workout ? workout.exercises.map((exercise, idx) => handleSets(exercise, idx)) : null;
@@ -135,12 +117,8 @@ export default function WorkoutDetail({ workout, routineInfo, currRoutine, setWo
         if (!workout) return null;
         workout.exercises.forEach((exercise, idx) => {
             if (completeArr[idx].every(ele => ele == true)) {
-                console.log(exercise.name + ' was fully completed')
-                console.log(`${exercise.name}'s weight was ${exercise.weight}.`)
-                console.log(currRoutine._id);
                 routineAPI.handleROG(currRoutine._id, workoutIdx, idx);
-                console.log(`${exercise.name}'s weight is now ${exercise.weight}.`)
-            } else console.log(exercise.name + ' was not fully completed')
+            }
         })
     }
 
@@ -158,7 +136,6 @@ export default function WorkoutDetail({ workout, routineInfo, currRoutine, setWo
             <Link to="/home">
                 <Fab variant="extended" onClick={async () => {
                     await routineInfoAPI.dayCompleted();
-                    console.log('fab click')
                     await routineInfoAPI.getUserRoutineInfo().then((value) => {
                         setRoutineInfo(value);
                     });
